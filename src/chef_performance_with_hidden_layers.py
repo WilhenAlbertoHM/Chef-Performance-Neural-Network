c36913699aab34430b4901e5f1f829993eee1f13
Wilhen Alberto Hui Mei <92064680+WilhenAlbertoHM@users.noreply.github.com>
2023-12-31 16:03:02 -0500

    Add files via upload

---
+++ src/chef_performance_with_hidden_layers.py
@@ -0,0 +1,88 @@
+import pickle
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, BatchNormalization, Dropout
+from sklearn.preprocessing import MinMaxScaler
+
+# Read the dataset file chef_data.pickle
+with open("data/chef_data.pickle", "rb") as f:
+    (x_train, y_train), (x_test, y_test) = pickle.load(f)
+
+print("x_train shape: ", x_train.shape)
+print("x_test shape: ", x_test.shape)
+print("y_train shape: ", y_train.shape)
+print("y_test shape: ", y_test.shape)
+
+# Define the Keras Sequential model
+model = Sequential()
+
+# Use MinMaxScaler to scale y-values before using them for 
+# training/testing with same scale factor
+scaler = MinMaxScaler()
+y_train_scaled = scaler.fit_transform(y_train)
+y_test_scaled = scaler.fit_transform(y_test)
+
+# Build model with an input and an ouput layer (no hidden layers)
+# 20 input units, batch normalization layer, 12 neurons in between, 
+# dropout layer, and 4 output
+model.add(Dense(units=20, input_dim=20, activation="relu"))
+model.add(BatchNormalization())
+model.add(Dense(units=12, activation="tanh"))
+model.add(Dropout(0.1))
+model.add(Dense(units=4))
+
+# Compile the Keras model with MSE and Adam optimizer
+model.compile(loss="mse", optimizer="adam")
+
+# Print summary of model
+print("|==============================| Summary |==============================|")
+model.summary()
+
+# Train model with 200 epochs and a batch size of 32.
+num_epochs = 200
+batch_size = 32
+model.fit(x_train, y_train_scaled, epochs=num_epochs, batch_size=batch_size)
+
+# Evaluate the model by predicting the outputs and rescale them back to see actual predictions
+y_pred_scaled = model.predict(x_test)
+y_pred = scaler.inverse_transform(y_pred_scaled)
+
+# Calculate mean and standard deviation of the difference between predictions and actual values 
+# across all test samples for each of the four variables to get an idea of how well it works.
+# Take mean and standard deviation over axis = 0.
+y_diff = np.abs(np.subtract(y_pred, y_test))
+avgs = np.average(y_diff, axis=0)
+std_devs = np.std(y_diff, axis=0)
+
+# Evaluate the test and train losses.
+train_loss = model.evaluate(x_train, y_train_scaled)
+test_loss = model.evaluate(x_test, y_test_scaled)
+
+# Print the number of epochs, activation function, and the loss function used.
+print()
+print("|================================================================================|")
+print("Number of epochs used: ", num_epochs)
+print("Batch size: ", batch_size)
+print("Activation function used: relu and tanh")
+print("Loss function used: MSE")
+
+# Print final MSE and accuracy for test and train losses.
+print()
+print("|================================================================================|")
+print("Train MSE loss: ", train_loss)
+print("Test MSE loss: ", test_loss)
+
+# Print the averages and standard deviations
+print()
+print("|================================================================================|")
+print("Averages of the variables: ", avgs)
+print("Standard deviation values of the variables", std_devs)
+
+# Print the outputs and check the averages with its standard deviation
+print()
+print("|================================================================================|")
+print("Number of tasks completed: ", str(round(avgs[0], 2)) + " +- " + str(round(std_devs[0], 2)) + " -> ~" + str(round(avgs[0])) + " task(s)")
+print("Number of tasks not completed: ", str(round(avgs[1], 2)) + " +- " + str(round(std_devs[1], 2)) + " -> ~" + str(round(avgs[1])) + " task(s)")
+print("Average bonus ($) each chef earned after the project: ", str(round(avgs[2], 2)) + " +- " + str(round(std_devs[2], 2)))
+print("Money ($) that the company earned/lost after the project: ", str(round(avgs[3], 2)) + " +- " + str(round(std_devs[3], 2)))
+print()
